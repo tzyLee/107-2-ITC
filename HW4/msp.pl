5e856539f5b2270f87cec8580b6af335927e7466
tzyLee <b06901145@ntu.edu.tw>
2019-04-29 23:17:05 +0800

    Refactor code

--- HW4/msp.pl
+++ HW4/msp.pl
@@ -1,21 +1,20 @@
-/* Use 0 if Result is negative */
 maxSum(X, Sum):-
     maxSumImpl(X, _, Sum).
-/* Reversed kadane's algorithm (Tail recursive) */
+/* Reversed Kadane's algorithm (Tail recursive) */
 /* Base case
- maxSumStartWithHead([], 0).
-   Maximum subarray sum starts with X = X or X + maxSumStartWithHead(Xs)
- maxSumStartWithHead([X|Xs], MaxStartWithX):-
-     maxSumStartWithHead(Xs, MaxAfterX),
-	 MaxStartWithX is max(X, X+MaxAfterX).
+maxSumStartWithHead([], 0).
+Maximum subarray sum starts with X = X or X + maxSumStartWithHead(Xs)
+maxSumStartWithHead([X|Xs], MaxStartWithX):-
+    maxSumStartWithHead(Xs, MaxAfterX),
+    MaxStartWithX is max(X, X+MaxAfterX).
 */
 
 maxSumImpl([], 0, 0).
 maxSumImpl([X|Xs], MaxStartWithX, OverallMax):-
     maxSumImpl(Xs, MaxAfterX, OverallMaxWithoutX),
-	MaxStartWithX is max(X, X+MaxAfterX),
-	OverallMax is max(OverallMaxWithoutX, MaxStartWithX).
-	
+    MaxStartWithX is max(X, X+MaxAfterX),
+    OverallMax is max(OverallMaxWithoutX, MaxStartWithX).
+
 /* Tests
 
 maxSum([], A).
