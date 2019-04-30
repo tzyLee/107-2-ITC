d6cc6121b6e8483041353f526d9ac78b69a2f975
tzyLee <b06901145@ntu.edu.tw>
2019-04-30 10:23:02 +0800

    Add bonus.cpp

---
+++ HW4/bonus.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+#define HEIGHT (n + 1)
+#define WIDTH (sum / 2 + 1) // Only half is needed
+#define _ind(i, j) ((i)*WIDTH + (j))
+#define absDiff(a, b) ((a) > (b) ? (a) - (b) : (b) - (a))
+    // Initialize
+    std::ifstream ifs("input.txt");
+    std::vector<unsigned> tasks;
+    unsigned long long sum = 0;
+    unsigned n;
+    ifs >> n;
+    tasks.reserve(n);
+    for (unsigned i = 0, temp = 0; i < n; ++i)
+    {
+        ifs >> temp;
+        tasks.push_back(temp);
+        sum += temp;
+    }
+    // Row: [0..n/2] col: [0..n/2]
+    bool *table = new bool[WIDTH * HEIGHT];
+    // Pseudo polynomial time algorithm
+    // table[i, j]: there is a subset of [0..i-1] with sum equals to j
+    for (unsigned i = 1; i < HEIGHT; ++i) // sum 0 is always possible
+        table[_ind(i, 0)] = true;
+    std::fill(table + 1, table + WIDTH, false); // When subset is [0..0], only sum 0 is possible
+    for (unsigned i = 1; i < HEIGHT; ++i)
+        for (unsigned j = 1; j < WIDTH; ++j)
+        {
+            // [0..i-1] have a subset with sum <=>
+            table[_ind(i, j)] = table[_ind(i - 1, j)];
+            // [0..i-2] have a subset with sum = sum
+            if (tasks[i - 1] <= j) // Non-negative number
+                table[_ind(i, j)] |= table[_ind(i, j - tasks[i - 1])];
+            // or [0..i-2] have a subset with sum = sum - tasks[i-1]
+        }
+    unsigned long long largestPossibleSum = 0;
+    for (unsigned j = WIDTH; j > 0; --j)
+        if (table[_ind(n, (largestPossibleSum = j - 1))])
+            break;
+    std::cout << absDiff(largestPossibleSum, sum - largestPossibleSum) << std::endl;
+    std::vector<unsigned> group2;
+    group2.reserve(sum - largestPossibleSum);
+    // Backtrace
+    unsigned i = n, j = largestPossibleSum;
+    while (i > 0 || j != 0)
+    {
+        // table[i, j] is true
+        if (table[_ind(i - 1, j)]) // not include tasks[i - 1], group 2
+        {
+            --i;
+            group2.push_back(tasks[i - 1]);
+        }
+        else if (j >= tasks[i - 1] && table[_ind(i, j - tasks[i - 1])]) // include tasks[i-1], group1
+        {
+            j -= tasks[i - 1];
+            std::cout << tasks[i - 1] << ' ';
+        }
+    }
+    std::cout.put('\n');
+    for (const unsigned &i : group2)
+        std::cout << i << ' ';
+    delete[] table;
+    return 0;
+}
